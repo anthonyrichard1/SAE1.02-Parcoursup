5275a85453a9da646b5040dd84ef9d0c62cf29f9
Anthony RICHARD <anthony.richard@etu.uca.fr>
2022-12-14 09:53:18 +0100

    ajout des fichiers structures.h structures.c testStructures.c / ajout des structures dans src/Makefile \ ajout des structures MaillonDept, ListeDept et VilleIUT

---
+++ src/structures.h
@@ -0,0 +1,52 @@
+/**
+ * \file structures.h
+ * \author Anthony RICHARD
+ * \author Léana BESSON
+ * \date 14/12/2022
+*/
+
+typedef struct MaillonDept MaillonDept;
+
+/**
+ * \struct MaillonDept structures.h
+ * \brief Cette structure représente un département.
+ * \param departement Chaîne de 30 caractères - Nom du département.
+ * \param nbP Entier - Nombre de places en première année.
+ * \param nomRes Chaîne de caractères - Nom du responsable du département.
+ * \param suivant Pointeur sur un département - Le département suivant.
+*/
+struct MaillonDept
+{
+    char departement[30];
+    int nbP;
+    char *nomRes;
+    MaillonDept *suivant;
+};
+
+typedef struct ListeDept *ListeDept;
+
+/**
+ * \struct ListeDept structures.h
+ * \brief Cette structure représente une liste de départements.
+ * \param premier Pointeur sur un département - Le premier département de la liste.
+ * \param dernier Pointeur sur un département - Le dernier département de la liste.
+ * \param nb Entier - Nombre de départements dans la liste.
+*/
+struct ListeDept
+{
+    MaillonDept *premier;
+    MaillonDept *dernier;
+    int nb;
+};
+
+/**
+ * \struct VilleIUT structures.h
+ * \brief Cette structure représente une ville où il y a au moins 1 IUT.
+ * \param ville Chaîne de 30 caractères - Le nom de la ville.
+ * \param ldept Liste de départements - La liste des départements présents dans la ville.
+*/
+typedef struct
+{
+    char ville[30];
+    ListeDept ldept;
+}VilleIUT;
