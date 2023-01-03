66428111a16863a39bbea327e537b621aeb19c21
Anthony RICHARD <anthony.richard@etu.uca.fr>
2023-01-03 19:10:32 +0100

    Doxygen administrateurs.h

--- src/administrateurs.h
+++ src/administrateurs.h
@@ -1,8 +1,30 @@
+/**
+ * \file structures.h
+ * \author Anthony RICHARD
+ * \author Léana BESSON
+ * \date 14/12/2022
+*/
 #include "structures.h" 
 #include "menus.h"
 
+/**
+ * \brief Cette fonction permet d'ajouter un IUT dans un tableau d'IUT, qui est ensuite renvoyé.
+ * \param **tiut Tableau de pointeurs sur des VilleIUT - le tableau dans lequel il faut ajouter la ville.
+ * \param *nbIUT Pointeur sur un entier - le nombre d'IUT contenus dans le tableau (taille logique de tiut).
+ * \returns VilleIUT - La nouvelle adresse du tableau passé en argument.
+ */
 VilleIUT **ajouterIUT(VilleIUT **tiut, int *nbIUT);
 
+/**
+ * \brief Cette fonction permet de supprimer un IUT dans un tableau de pointeurs sur des VilleIUT.
+ * \param **tiut Tableau de pointeurs sur des VilleIUT - le tableau dans lequel il faut supprimer la ville.
+ * \param *nbIUT Pointeur sur un entier - le nombre d'IUT contenus dans le tableau (taille logique de tiut).
+ */ 
 void supprimerIUT(VilleIUT **tiut, int *nbIUT);
 
+/**
+ * \brief Cette fonction de supprimer un département dans un tableau de pointeurs sur des VilleIUT.
+ * \param **tiut Tableau de pointeurs sur des VilleIUT - le tableau dans lequel il faut supprimer la ville.
+ * \param *nbIUT Pointeur sur un entier - le nombre d'IUT contenus dans le tableau (taille logique de tiut).
+ */
 void supprimerDepart(VilleIUT **tiut, int *nbIUT);
