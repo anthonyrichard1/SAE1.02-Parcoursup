dd8835098714ce2ea74d55bba2fe709c8e25f22a
Anthony RICHARD <anthony.richard@etu.uca.fr>
2023-01-04 08:37:09 +0100

    Doxygen des fichiers administrateurs.h et informations.h + inclusion de erreur.h dans Doxygen (le nom du fichier était incorrect et donc pas lu par Doxygen)

--- src/administrateurs.h
+++ src/administrateurs.h
@@ -1,8 +1,9 @@
 /**
- * \file structures.h
+ * \file administrateurs.h
  * \author Anthony RICHARD
  * \author Léana BESSON
  * \date 14/12/2022
+ * \brief Dans ce fichier sont répertoriées toutes les fonctions des administrateurs.
 */
 #include "structures.h" 
 #include "menus.h"
@@ -11,7 +12,7 @@
  * \brief Cette fonction permet d'ajouter un IUT dans un tableau d'IUT, qui est ensuite renvoyé.
  * \param **tiut Tableau de pointeurs sur des VilleIUT - le tableau dans lequel il faut ajouter la ville.
  * \param *nbIUT Pointeur sur un entier - le nombre d'IUT contenus dans le tableau (taille logique de tiut).
- * \returns VilleIUT - La nouvelle adresse du tableau passé en argument.
+ * \return VilleIUT - La nouvelle adresse du tableau passé en argument.
  */
 VilleIUT **ajouterIUT(VilleIUT **tiut, int *nbIUT);
 
